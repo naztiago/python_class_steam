ad6b718d28514c8d6f0d1f4b927aa6ebd3cf66fd
naztiago <naztiago@gmail.com>
2023-02-07 20:39:54 -0500

    ahora si se puede ejcutar la funcion

--- main.py
+++ main.py
@@ -1,42 +1,4 @@
-import math
-
-def area_rectangulo(
-    x:float
-    ,y:float
-): 
-
-    ''' 
-    Funcion que calcula el area de un rectangulo
-
-    ---params---
-    - x (float): es la base del rectangulo
-    - y (float): es la altura del rectangulo
-
-    ---return---
-    - area (float): es el area del rectangulo
-    '''
-    area = x*y
-    return area
-
-def distancia_entre_dos_puntos(
-    x_1:float,
-    x_2:float,
-    y_1:float,
-    y_2:float
-) ->float:
-    '''
-    Funcion para calcular la distancia entre dos puntos
-
-    ---params---
-    - x_1 (float): punto inicial de la base
-    - x_2 (float): punto final de la base
-    - y_1 (float): punto inicial de la altura
-    - y_2 (float): punto final de la altura
-
-    ---return---
-    - d (float): distancia entre 2 puntos
-    '''
-    x = math.pow(x_2-x_1,2)
-    y = math.pow(y_2-y_1,2)
-    d = math.sqrt(x+y)
-    return d
+import funciones
+x = int(input('dame la base: '))
+y = int(input('dame la altura: '))
+print('El area de tu rectangulo es: ',funciones.area_rectangulo(x,y))
